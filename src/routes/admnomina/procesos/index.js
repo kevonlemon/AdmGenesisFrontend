108106ae2d94447ab1ea04d2848ae436d087dc25
ASUS_Birobid <jcaicedo@birobid.com>
2022-08-05 16:30:49 -0500

    Modulo Beneficio Social terminado

--- src/routes/admnomina/procesos/index.js
+++ src/routes/admnomina/procesos/index.js
@@ -4,9 +4,15 @@ import { Loadable } from '../../../utils/loadable';
 // ========================== INICIO ===================================
 const Inicio = Loadable(lazy(() => import('../../../sections/admnomina/inicio/inicio')));
 
+const Beneficiosocial = Loadable(lazy(() => import('../../../sections/admnomina/procesos/beneficiosocial/beneficiosocial')));
+
 export const PROCESOS = [
+    // {
+    //     url: '/dashboard',
+    //     element: <Inicio />
+    // },
     {
-        url: '/dashboard',
-        element: <Inicio />
+        url: '/beneficiosocial',
+        element: <Beneficiosocial />
     }
 ]
